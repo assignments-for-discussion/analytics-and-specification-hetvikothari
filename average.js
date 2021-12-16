3e9a8da00fa5fb2da408e4c692d0464da589b04d
Hetvi Kothari <59480054+hetvikothari@users.noreply.github.com>
2021-12-16 20:55:25 +0530

    Test to check for outliers added

--- average.js
+++ average.js
@@ -1,9 +1,30 @@
+function filterOutliers(nums) {
+  if(nums.length < 4)
+    return nums;
+  let values, q1, q3, iqr, maxValue, minValue;
+  values = nums.slice().sort( (a, b) => a - b);
+  if((values.length / 4) % 1 === 0){
+  //find quartiles
+    q1 = 1/2 * (values[(values.length / 4)] + values[(values.length / 4) + 1]);
+    q3 = 1/2 * (values[(values.length * (3 / 4))] + values[(values.length * (3 / 4)) + 1]);
+  } else {
+    q1 = values[Math.floor(values.length / 4 + 1)];
+    q3 = values[Math.ceil(values.length * (3 / 4) + 1)];
+  }
+  iqr = q3 - q1;
+  maxValue = q3 + iqr * 1.5;
+  minValue = q1 - iqr * 1.5;
+  return values.filter((x) => (x >= minValue) && (x <= maxValue));
+}
+
+
 function average(numbers) {
     if (numbers.length == 0) {
         return NaN
     } else {
         numbers = numbers.filter((num) => !Number.isNaN(num));
-        return numbers.reduce((p, c) => p + c, 0) / numbers.length;
+        numberWithoutOutliers = filterOutliers(numbers)
+        return numberWithoutOutliers((p, c) => p + c, 0) / numberWithoutOutliers.length;
     }
 }
 module.exports = {average};
