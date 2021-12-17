353bebb58181e609308a03cd981b3b27aa5af59b
Hetvi Kothari <59480054+hetvikothari@users.noreply.github.com>
2021-12-17 20:57:48 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,21 +1,21 @@
 function filterOutliers(nums) {
   if (nums.length < 4)
     return nums;
-  let values = nums.slice().sort((a, b) => a - b);
+  var values = nums.slice().sort((a, b) => a - b);
   if ((values.length / 4) % 1 === 0) {
     //find quartiles
-    const q1 = 1 / 2 * (values[(values.length / 4)] +
+    var q1 = 1 / 2 * (values[(values.length / 4)] +
       values[(values.length / 4) + 1]);
-    const q3 = 1 / 2 * (values[(values.length * (3 / 4))] +
+    var q3 = 1 / 2 * (values[(values.length * (3 / 4))] +
       values[(values.length * (3 / 4)) + 1]);
   } else {
-    const q1 = values[Math.floor(values.length / 4 + 1)];
-    const q3 = values[Math.ceil(values.length * (3 / 4) + 1)];
+    var q1 = values[Math.floor(values.length / 4 + 1)];
+    var q3 = values[Math.ceil(values.length * (3 / 4) + 1)];
   }
-  const iqr = q3 - q1;
-  const maxValue = q3 + iqr * 1.5;
-  const minValue = q1 - iqr * 1.5;
-  return values.filter((x) => (x >= minValue) && (x <= maxValue));
+  var iqr = q3 - q1;
+  var maxValue = q3 + iqr * 1.5;
+  var minValue = q1 - iqr * 1.5;
+  var values.filter((x) => (x >= minValue) && (x <= maxValue));
 }
 
 
